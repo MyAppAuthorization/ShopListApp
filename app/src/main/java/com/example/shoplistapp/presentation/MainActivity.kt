48990298eef9bc1e38458adf39a049ecf00052d7
Name <korobkova.olja@gmail.com>
2023-06-23 11:17:03 +0300

    Создание Presentation слоя

--- app/src/main/java/com/example/shoplistapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplistapp/presentation/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.shoplistapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoplistapp.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this){
+            Log.d("MainActivityTest", it.toString())
+        }
     }
 }
---
+++ app/src/main/java/com/example/shoplistapp/presentation/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.shoplistapp.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoplistapp.data.ShopListRepositoryImpl
+import com.example.shoplistapp.domain.DeleteShopItemUseCase
+import com.example.shoplistapp.domain.EditShopItemUseCase
+import com.example.shoplistapp.domain.GetShopListUseCase
+import com.example.shoplistapp.domain.ShopItem
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+
+    }
+
+    fun chengeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enable = !shopItem.enable)
+        editShopItemUseCase.editShopItem(newItem)
+
+
+    }
+
+}
