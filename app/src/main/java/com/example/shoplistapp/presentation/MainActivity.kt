680e640f0ff14eeb9b05246b44b8c532c5aaf749
Name <korobkova.olja@gmail.com>
2023-06-27 15:47:52 +0300

    Реализация списков через LinearLayout

--- app/src/main/java/com/example/shoplistapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplistapp/presentation/MainActivity.kt
@@ -3,17 +3,43 @@ package com.example.shoplistapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoplistapp.R
+import com.example.shoplistapp.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var llShopList: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this){
-            Log.d("MainActivityTest", it.toString())
+            showList(it)
+        }
+    }
+    private fun showList(list: List<ShopItem>){
+        llShopList.removeAllViews()
+        for (shopItem in list){
+            val layoutId = if (shopItem.enable){
+                R.layout.item_shop_enabled
+            } else{
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener{
+                viewModel.chengeEnableState(shopItem)
+                true
+            }
+            llShopList.addView(view)
         }
     }
 }
