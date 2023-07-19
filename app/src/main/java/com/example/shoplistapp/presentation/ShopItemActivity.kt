fe646732f91b5968cdd9cdb411e592f6562c3e51
Name <korobkova.olja@gmail.com>
2023-07-19 16:23:09 +0300

    Завершение работы над ShopItemViewModel

--- app/src/main/java/com/example/shoplistapp/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoplistapp/presentation/ShopItemActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.shoplistapp.R
 
 class ShopItemActivity: AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        viewModel.errorInputName.value = false
     }
 }
